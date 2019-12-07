fb8e12464119bca841cb11f0825124cac9f7bed6
ajit.tomar <ajit.tomar@.infosys.com>
2019-12-07 23:29:22 +0530

    Manju second commit

--- TestProject/src/testPackage/PrintHelloName.java
+++ TestProject/src/testPackage/PrintHelloName.java
@@ -2,6 +2,6 @@ package testPackage;
 
 public class PrintHelloName {
 public static void main(String[] args) {
-	System.out.println("Hello Ajit singh Tomar and Manju Veerla and gunjan");
+	System.out.println("Hello Ajit singh Tomar and Manju Veerla and gunjan agrawal");
 }
 }
