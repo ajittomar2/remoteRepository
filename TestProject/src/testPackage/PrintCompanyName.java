badaa7104e528c7e2fc05329a853ddf284cea7f5
ajit.tomar <ajit.tomar@.infosys.com>
2019-12-08 22:55:50 +0530

    Added company name with location

--- TestProject/src/testPackage/PrintCompanyName.java
+++ TestProject/src/testPackage/PrintCompanyName.java
@@ -2,6 +2,6 @@ package testPackage;
 
 public class PrintCompanyName {
 public static void main(String[] args) {
-	System.out.println("Infosys limited");
+	System.out.println("Infosys limited hyderabad");
 }
 }
