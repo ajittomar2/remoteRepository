034d984f8238334d450fc6b5b4864bb96a7383d8
ajit.tomar <ajit.tomar@.infosys.com>
2019-12-08 23:08:01 +0530

    Added loop

--- TestProject/src/testPackage/PrintCompanyName.java
+++ TestProject/src/testPackage/PrintCompanyName.java
@@ -2,6 +2,9 @@ package testPackage;
 
 public class PrintCompanyName {
 public static void main(String[] args) {
+	for (int i = 0; i < 10; i++) {
+		System.out.println("loop number - "+i);
+	}
 	System.out.println("Infosys limited hyderabad");
 }
 }
