ec912fcb0f1a60c0f1a90bbef5d494ff6020d309
ajit.tomar <ajit.tomar@.infosys.com>
2019-12-09 17:23:02 +0530

    Modified company name - devloper two

--- TestProject/src/testPackage/PrintCompanyName.java
+++ TestProject/src/testPackage/PrintCompanyName.java
@@ -3,8 +3,8 @@ package testPackage;
 public class PrintCompanyName {
 public static void main(String[] args) {
 	for (int i = 0; i < 10; i++) {
-		System.out.println("loop number - "+i);
+		System.out.println("loop no - "+i);
 	}
-	System.out.println("Infosys limited hyderabad");
+	System.out.println("Infosys limited hyderabad...");
 }
 }
