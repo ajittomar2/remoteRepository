a0c1603824bd5cadc60f66982765d77d93e09cc7
ajit.tomar <ajit.tomar@.infosys.com>
2019-12-09 11:03:44 +0530

    first commit in sub branch one

--- TestProject/src/testPackage/PrintCompanyName.java
+++ TestProject/src/testPackage/PrintCompanyName.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
 	for (int i = 0; i < 10; i++) {
 		System.out.println("loop number - "+i);
 	}
-	System.out.println("Infosys limited hyderabad");
+	System.out.println("Infosys limited");
 }
 }
